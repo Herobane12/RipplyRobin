919ace751add1418d5ce3c40ff67dd9fe01d3bdc
MUKILTEO\1605074 <193661@apps.everettsd.org>
2025-02-25 13:29:34 -0800

    something2

--- 2D Template/Assets/CameraSwitcher.cs
+++ 2D Template/Assets/CameraSwitcher.cs
@@ -2,14 +2,31 @@ using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour
 {
+    public GameObject camObject1, camObject2;
     bool cam = true;
     bool cam2 = false;
+
+    public bool isActive;
+    public GameObject Button;
 
     void Start()
     {
         bool cam = true;
         bool cam2 = false;
     }
+
+    public void Activate()
+    {
+        Button.SetActive(true);
+        isActive = true;
+    }
+
+    public void DeActivate()
+    {
+        Button.SetActive(false);
+        isActive = false;
+    }
+
 
     void SwapCamera()
     {
@@ -18,5 +35,11 @@ public class CameraSwitcher : MonoBehaviour
             bool cam = false;
             bool cam2 = true;
         
+    }
+
+    private void Update()
+    {
+        camObject1.SetActive(cam);
+        camObject2.SetActive(cam2);
     }
 }
